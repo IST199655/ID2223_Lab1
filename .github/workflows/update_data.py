ddce15a67b526c79e5d5ad3f3c8b0fb13f870648
desert <desert@kth.se>
2024-11-14 10:24:21 +0100

    fix update daily go to ml algo

--- .github/workflows/update_data.py
+++ .github/workflows/update_data.py
@@ -8,22 +8,29 @@ import warnings
 
 ## GET THE AIR QUALITY FORECAST FOR THE NEXT DAYS
 
-# Here aqi key is not a secret 
+# Here hopsworks key is not a secret 
 
-aqi_api_key_file = 'aqi-api-key.txt'
-
-with open(aqi_api_key_file, 'r') as file:
-    AQI_API_KEY = file.read().rstrip()
+with open('../../data/hopsworks-api-key.txt', 'r') as file:
+    os.environ["HOPSWORKS_API_KEY"] = file.read().rstrip()
 
 ## Here it is
 
-# # Get the API key from GitHub Secrets
-# AQI_API_KEY = os.getenv('AQI_API_KEY')
+# Get the API key from GitHub Secrets
+#HOPSWORKS_API_KEY = os.getenv('HOPSWORKS_API_KEY')
 
-# if AQI_API_KEY is None:
-# raise ValueError("API key is missing. Set it in GitHub Secrets.")
+#Get AQI API KEY from secrets of hopsworks
 
-# TODO: Change these values to point to your Sensor
+proj = hopsworks.login(project="ID2223LAB1KTH")
+
+conn = hopsworks.connection(host="c.app.hopsworks.ai", project=proj, api_key_value=os.environ.get('HOPSWORKS_API_KEY'))
+secrets = conn.get_secrets_api()
+
+AQI_API_KEY = secrets.get_secret("AQI_API_KEY").value
+
+
+
+
+
 country="sweden"
 city = "stockholm"
 street = "stockholm-hornsgatan-108"
@@ -40,7 +47,6 @@ if response.status_code == 200:
     # 5. Parse the data 
     pm25_data = data.get("data", {}).get("forecast", {}).get("daily", {}).get("pm25", [])
     pm10_data = data.get("data", {}).get("forecast", {}).get("daily", {}).get("pm10", [])
-    no2_data = data.get("data", {}).get("forecast", {}).get("daily", {}).get("no2", [])
 
 
     pm25_df = pd.DataFrame(pm25_data)
@@ -50,8 +56,7 @@ if response.status_code == 200:
     pm10_df = pm10_df[['day', 'avg']].rename(columns={'avg': 'pm10'})
     
 
-
-    df_forecast_aq = pm25_df.merge(pm10_df, on="day", how="inner").drop(index=[0,1,2])
+    df_forecast_aq = pm25_df.merge(pm10_df, on="day", how="inner").drop(index=0)
     df_forecast_aq[['pm25', 'pm10']] = df_forecast_aq[['pm25', 'pm10']].astype(str)
    
     df_forecast_aq['day'] = pd.to_datetime(df_forecast_aq['day'])
@@ -66,8 +71,8 @@ longitude = 18.0686
 
 # Get the current date and calculate the forecast date range (e.g., next 7 days)
 today = datetime.today().date()
-forecast_start_date = today
-forecast_end_date = today + timedelta(days=7)  # 7 days from today
+forecast_start_date = today - timedelta(days=1)
+forecast_end_date = today + timedelta(days=5)  # 7 days from today
 
 # URL for Open Meteo forecast
 forecast_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&start_date={forecast_start_date}&end_date={forecast_end_date}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m,wind_gusts_10m"
@@ -88,9 +93,7 @@ df_forecast_weather = pd.DataFrame({
 
 # UPDATE THE FEATURE GROUPS
 
-project = hopsworks.login(project = "ID2223LAB1KTH")
-
-fs = project.get_feature_store()
+fs = proj.get_feature_store()
 
     # Define the Feature Group for Forecast Air Quality Data
 feature_group_name = "stockholm_weather"
