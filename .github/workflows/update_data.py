048e81e67f95232b87da670e0b2ebc8069211e85
desert <desert@kth.se>
2024-11-14 13:29:57 +0100

    fix commentary

--- .github/workflows/update_data.py
+++ .github/workflows/update_data.py
@@ -14,13 +14,13 @@ from retry_requests import retry
 
 # Here hopsworks key is not a secret 
 
-with open('../../data/hopsworks-api-key.txt', 'r') as file:
-     os.environ["HOPSWORKS_API_KEY"] = file.read().rstrip()
+#with open('../../data/hopsworks-api-key.txt', 'r') as file:
+#    os.environ["HOPSWORKS_API_KEY"] = file.read().rstrip()
 
 ## Here it is
 
 # Get the API key from GitHub Secrets
-#HOPSWORKS_API_KEY = os.getenv('HOPSWORKS_API_KEY')
+HOPSWORKS_API_KEY = os.getenv('HOPSWORKS_API_KEY')
 
 #Get AQI API KEY from secrets of hopsworks
 
